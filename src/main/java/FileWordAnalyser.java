167ab753346b0e390c56188768d5aca046f8f187
KozubIgn <ignacykozub@gmail.com>
2020-07-19 18:15:43 +0200

    add FileWordAnalyser

---
+++ src/main/java/FileWordAnalyser.java
@@ -0,0 +1,59 @@
+import java.io.IOException;
+import java.util.*;
+
+public class FileWordAnalyser {
+    private FilePartReader filePartReader;
+
+    public FileWordAnalyser(FilePartReader filePartReader) {
+        this.filePartReader = filePartReader;
+    }
+
+    public List<String> getWordsOrderedAlphabetically() throws IOException {
+        String[] arrayOfWords = getWordsArray();
+        List<String> list = Arrays.asList(arrayOfWords);
+        Collections.sort(list);
+        return list;
+    }
+
+    public List<String> getWordsContainingSubstring(String subString) throws IOException {
+        String[] arrayOfWords = getWordsArray();
+        List<String> listOfSubstrings = new ArrayList<>();
+
+        for (String word : arrayOfWords) {
+            if (word.contains(subString)) {
+                listOfSubstrings.add(word);
+            }
+        }
+        return listOfSubstrings;
+    }
+
+    public List<String> getStringWhichPalindromes() throws IOException {
+        String[] arrayOfWords = getWordsArray();
+        List<String> listOfPalindromes = new ArrayList<>();
+
+        for (String s : arrayOfWords) {
+            char[] wordInChars = s.toCharArray();
+            int firstIndex = 0;
+            int lastIndex = wordInChars.length - 1;
+
+            while (lastIndex > firstIndex) {
+                if (wordInChars[firstIndex] != wordInChars[lastIndex]) {
+                    break;
+                }
+                ++firstIndex;
+                --lastIndex;
+            }
+
+            if (lastIndex != wordInChars.length - 1) {
+                listOfPalindromes.add(s);
+            }
+        }
+        return listOfPalindromes;
+    }
+
+    public String[] getWordsArray() throws IOException {
+        String words = filePartReader.readLines();
+        return words.split(" ");
+    }
+}
+
